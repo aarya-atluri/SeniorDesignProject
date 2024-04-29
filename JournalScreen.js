7bd0d48e649c116f625ec8995e090c350e382a28
Lindsey Kim <lindsey.kim.081@k12.friscoisd.org>
2024-04-29 10:16:49 -0500

    fixed streaks

--- JournalScreen.js
+++ JournalScreen.js
@@ -1,262 +1,178 @@
-// JournalScreen.js
-import React, {useState} from 'react';
-import { Button, TouchableOpacity } from 'react-native';
-import { Image, TextInput, SafeAreaView, View, Text, StyleSheet, ScrollView} from 'react-native';
-import { useFonts } from 'expo-font';
-import ImagePicker from 'react-native-image-picker';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'; // Import TouchableOpacity
+import { Calendar } from 'react-native-calendars';
 import { useNavigation } from '@react-navigation/native';
-import {auth, db, collection} from './Firebase/firebaseConfig'
-import {doc, getDoc} from 'firebase/firestore'
+import { Ionicons } from '@expo/vector-icons';
+import {auth, db} from './Firebase/firebaseConfig'
+import {collection, query, where, doc, getDoc} from 'firebase/firestore'
 
 
-const JournalScreen = ({route}) => {
-  const[dailySleep, setSleep] = useState('')
-  const[dailyExercise, setExercise] = useState('')
-  const[dailyEntry, setDiary] = useState('')
-  const[dailyWater, setWater] = useState('')
-  const[dailyCaffeine, setCaffeine] = useState('')
-  const [selectedMood, setMood] = useState(null);
+const CalendarScreen = ({}) => {
+  const [currentMonth, setCurrentMonth] = useState(new Date().toISOString().slice(0, 7));
+  const [markedDates, setMarkedDates] = useState({});
+  const navigation = useNavigation(); // Initialize navigation
+
+  const handleDayPress = async (day) => {
+    // Navigate to a different screen upon pressing a specific date
+    const selectedDate = day.dateString;
   
-  const navigation = useNavigation();
-
-  const onImageHandler = (imageId) => {
-    setMood(imageId);
-  }
-
-  const onTextChangeHandler = (text) => {
-    setDiary(text);
-  };
-
-  const onSleepChangeHandler = (text) => {
-    setSleep(text);
-  };
-
-  const onExerciseChangeHandler = (text) => {
-    setExercise(text);
-  };
-
-  const onWaterChangeHandler = (text) => {
-    setWater(text);
-  };
-
-  const onCaffeineChangeHandler = (text) => {
-    setCaffeine(text);
-  };
-
-  const onPressHandler = async () => {
+  // Check if there are entries for the selected date
     try {
-      const todayDate = new Date().toLocaleDateString(); 
-      const entryRef = db.collection('journal_entries');
+    const moodRef = collection(db, 'journal_entries');
+    const q = query(moodRef, where('date', '==', selectedDate));
+    const querySnapshot = await getDoc(q);
+    
+    if (querySnapshot.size > 0) {
+      navigation.navigate('SpecificEntry', { selectedDate });
+    } else {
+      // Handle case where there are no entries for the selected date
+      console.log('No entries found for the selected date.');
+    }
+  } catch (error) {
+    console.error('Error fetching entries:', error);
+  }
+  };
 
-      await entryRef.add({
-        date: todayDate,
-        entry: dailyEntry,
-        sleep: dailySleep,
-        exercise: dailyExercise,
-        water: dailyWater,
-        caffeine: dailyCaffeine,
-        mood: selectedMood ? getImageUrl(selectedMood) : null 
+  const onPressHandler =() => {
+    navigation.navigate('Journal')
+  };
 
-      });
 
-    navigation.goBack();
-    } catch (error) {
-      console.error('Error saving entry:', error);
-      navigation.goBack();
+  useEffect(() => {
+    const fetchMoods = async () => {
+      try {
+        const moodRef = collection(db, 'journal_entries');
+        const querySnapshot = await getDoc(moodRef);
+        
+        const markedDatesObject = {};
+
+        querySnapshot.forEach(doc => {
+          const data = doc.data();
+          const date = data.date;
+          const mood = data.mood;
+
+          markedDatesObject[date] = { selected: true, selectedColor: getMoodColor(mood) };
+        });
+
+        setMarkedDates(markedDatesObject);
+      } catch (error) {
+        console.error('Error fetching moods:', error);
+      }
+    };
+
+    fetchMoods();
+  }, []);
+
+  const getMoodColor = (mood) => {
+    switch (mood) {
+      case 'excited':
+        return 'green';
+      case 'happy':
+        return 'yellow';
+      case 'meh':
+        return 'brown';
+      case 'sad':
+        return 'blue';
+      case 'dead':
+        return 'purple';
+      default:
+        return 'white';
     }
   };
 
-return (
+  const theme = {
+    textDayFontFamily: 'ProtestRiot-Regular',
+    textMonthFontFamily: 'ProtestRiot-Regular',
+    textDayHeaderFontFamily: 'ProtestRiot-Regular',
+    textDayFontSize: 16,
+    textMonthFontSize: 30,
+    textDayHeaderFontSize: 16,
+    todayTextColor: 'blue',
+    selectedDayBackgroundColor: 'green',
+    arrowColor: 'orange',
+    monthTextColor: '#d8e1e9',
+    'stylesheet.day.basic':{
+      'base':{
+        width:125,
+        height:40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 20,
+      },
+      'text': {
+        marginTop: 6,
+        fontSize: 16,
+        fontFamily: 'ProtestRiot-Regular',
+      }
+    }
+  };
+  return (
     <View style={styles.container}>
-      <Text style={styles.sectionTitle}>How Are You Feeling?</Text>
-      <View style={styles.sameRow}>
-        <TouchableOpacity onPress ={() => onImageHandler(1)}>
-        <Image
-          style={styles.image}
-          source={require('./assets/images/excited.jpg')} // Change the path to your image
-        />
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress ={() => onImageHandler(2)}>
-        <Image
-          style={styles.image}
-          source={require('./assets/images/happy.jpg')} // Change the path to your image
-        />
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress ={() => onImageHandler(3)}>
-        <Image
-          style={styles.image}
-          source={require('./assets/images/meh.jpg')} // Change the path to your image
-        />
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress ={() => onImageHandler(4)}>
-        <Image
-          style={styles.image}
-          source={require('./assets/images/sad.jpg')} // Change the path to your image
-        />
-        </TouchableOpacity>
-        <TouchableOpacity onPress ={() => onImageHandler(5)}> 
-        <Image
-          style={styles.image}
-          source={require('./assets/images/dead.jpg')} // Change the path to your image
-        />
-        </TouchableOpacity>
-      </View>
-      
-      <Text style ={styles.subSection}> New Journal Entry</Text>
-      <TextInput
-        multiline = {true}
-        placeholder = "Share your thoughts here"
-        onChangeText = {onTextChangeHandler}
-        value = {dailyEntry}
-        style = {styles.text}
+      <Text style={styles.sectionTitle}>Your Mindful Journal</Text>
+      <View style={styles.calendarContainer}>
+        <Calendar
+        current={currentMonth}
+        onMonthChange={(month) => setCurrentMonth(month.dateString)}
+        onDayPress={handleDayPress} // Handle press event for a specific 
+        theme = {theme}
+        style={{
+          borderRadius: 15,
+        }}
       />
-
-      <View style={styles.sameRow}>
-        <Text style ={styles.subSection}> Sleep</Text>
-        <TextInput 
-          placeholder = ""
-          onChangeText = {onSleepChangeHandler}
-          value = {dailySleep}
-          style = {styles.statistics}
-          />
       </View>
-
-      <View style={styles.sameRow}>
-          <Text style ={styles.subSection}> Exercise</Text>
-          <TextInput 
-          placeholder = ""
-          onChangeText = {onExerciseChangeHandler}
-          value = {dailyExercise}
-          style = {styles.statistics}
-          />
-      </View>
-    
-      <View style={styles.sameRow}>
-          <Text style ={styles.subSection}> Water </Text>
-          <TextInput 
-          placeholder = ""
-          onChangeText = {onWaterChangeHandler}
-          value = {dailyWater}
-          style = {styles.statistics}
-          />
-      </View>
-    
-      <View style={styles.sameRow}>
-          <Text style ={styles.subSection}> Caffeine</Text>
-          <TextInput
-          placeholder = ""
-          onChangeText = {onCaffeineChangeHandler}
-          value = {dailyCaffeine}
-          style = {styles.statistics}
-          />
-      </View>
-      
-      <View style= {styles.button}>
-          <Button 
-          title = "Check In" 
-          width = '150'
-          onPress={onPressHandler} 
-          color='#5d4632' />
-      </View>
-      
-  
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress= {onPressHandler}
+      >
+        <Ionicons name="ios-add" size={30} color="white" />
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollContainer: {
-    flexGrow: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: 'white',
-  },
   container: {
     flex: 1,
-    justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: 'white',
-  },
-  sameRow: {
-    flexDirection: 'row', // Arrange items horizontally
+    justifyContent: 'center',
   },
   sectionTitle: {
     fontSize: 40,
-    paddingTop: 60,
+    paddintTop: 20,
     paddingBottom: 40,
     fontWeight: '600',
-    fontFamily: 'ProtestRiot-Regular',
     color: '#5d4632',
-  },
-  image: {
-    width: 50,
-    height: 50,
-    margin: 5,
-    resizeMode: 'contain', // Adjust the resizeMode as per your requirement
-  },
-  topper: {
-    width: '100%',
-    height: '70%',
-    position: 'absolute'
-  },
-  overlayContainer: {
-    flex: 1,
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    position: 'absolute',
-    bottom: 0,
-    overflow: 'hidden',
-  },
-  overlay: {
-    width: '100%',
-    alignItems: 'center',
-    backgroundColor: 'rgba(255, 255, 255, 0.8)', // Adjust the opacity as per your requirement
-    paddingTop: 20,
-  },
-  subSection: {
-    fontSize: 20,
-    fontWeight: '600',
-    fontFamily: 'ProtestRiot-Regular',
-    paddingLeft: 100,
-    paddingBottom: 10,
-    alignSelf: 'flex-start',
-    color: '#5d4632',
-  },
-  statistics:{
-    borderBottomWidth: 1, // Add border bottom
-    borderColor: '#5d4632', // Set border color
-    paddingBottom: 5, // Add padding to create space between text and underline
-    paddingLeft: 20,
-    marginBottom: 10, // Add margin bottom to separate TextInput
-    alignSelf: 'flex-start',
-  },
-
-  button: {
-    flex: 1,
-    position: 'absolute',
-    alignSelf: 'center',
-    overflow: 'hidden',
-    width: 200,
-    height: 40,
-    borderRadius: 20,
-    fontFamily: 'ProtestRiot-Regular',
-    bottom: 40
+    fontFamily: 'ProtestRiot-Regular'
   },
   text: {
-    fontSize: 10,
+    fontSize: 40,
+    padding: 50,
+    fontWeight: '600',
     width: '80%',
-    height: 100,
-    padding: 10,
-    paddingBottom: 50,
+    height: 150,
+    letterSpacing: 0.25,
     fontFamily: 'ProtestRiot-Regular',
-    backgroundColor: '#d8e1e9',
-    borderColor: '#d8e1e9',
+    borderWidth: 1,
+    borderColor: '#5d4632',
     borderRadius: 15,
+  },
+  calendarContainer: {
+    width: '100%', // Set width to 100% to occupy full width of parent container
+    alignItems: 'center', // Center the calendar horizontally
+    marginTop: 20,
+  },
+  addButton: {
+    position: 'absolute',
+    bottom: 20,
+    alignSelf: 'center',
+    backgroundColor: '#8fbc8f',
+    borderRadius: 30,
+    width: 60,
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
-export default JournalScreen;
+export default CalendarScreen;
+
